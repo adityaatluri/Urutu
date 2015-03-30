d29687c1c0f6def4d9bba434a130b1655cf510d1
Aditya Atluri <adityaavinash1@gmail.com>
2015-03-30 15:57:40 -0400

    added profiling features

--- Urutu/cu/execu.py
+++ Urutu/cu/execu.py
@@ -3,7 +3,7 @@
 # Mail: pyurutu@gmail.com
 # This file contains the execution of CUDA code using PyCUDA
 # Modified: 18 Jan 2014
-
+import time
 import numpy
 
 class cu_exe:
@@ -16,6 +16,10 @@ class cu_exe:
 	nam_args = []
 	id_ret = []
 	is_alloc = []
+	malloc_times = []
+	htod_times = []
+	dtoh_times = []
+	exec_times = []
 	def exe_cu(self,stringg,func_name,threads,blocks,dyn_p,is_shared):
 		try:
 			import pycuda.driver as cuda
@@ -28,10 +32,13 @@ class cu_exe:
 		else:
 			mod=SourceModule(stringg)
 		func=mod.get_function(func_name)
+		start = time.time()
 		if is_shared == True:
 			func(*self.cu_args,block=(threads[0],threads[1],threads[2]),grid=(blocks[0],blocks[1],blocks[2]),shared=16*1024)
 		else:
 			func(*self.cu_args,block=(threads[0],threads[1],threads[2]),grid=(blocks[0],blocks[1],blocks[2]))
+		end = time.time()
+		self.exec_times.append(end - start)
 
 	def malloc(self,args,arg_nam):
 		try:
@@ -43,7 +50,10 @@ class cu_exe:
 		self.nam_args = arg_nam
 		self.argl = len(args)
 		for i in range(len(args)):
+			start = time()
 			self.cu_args.append(cuda.mem_alloc(args[i].nbytes))
+			end = time()
+			self.malloc_times.append(end - start)
 		
 	def htod(self,arg_nam):
 		try:
@@ -52,7 +62,10 @@ class cu_exe:
 		except:
 			return
 		index = self.nam_args.index(arg_nam)
+		start = time.time()
 		cuda.memcpy_htod(self.cu_args[index],self.args[index])
+		end = time.time()
+		self.htod_append(end - start)
 
 	def flags(self):
 		for i in self.nam_args:
@@ -77,7 +90,7 @@ class cu_exe:
 			ret_id = self.nam_args.index(i)
 			self.returns.append(self.args[ret_id])	
 		self.dfree()
-		return self.returns
+		return self.malloc_times, self.htod_times, self.exec_times, self.dtoh_times
 
 	def dtoh(self,nam):
 		try:
@@ -86,7 +99,10 @@ class cu_exe:
 		except:
 			return
 		var_id = self.nam_args.index(nam)
-		cuda.memcpy_dtoh(self.args[var_id], self.cu_args[var_id]);
+		start = time.time()
+		cuda.memcpy_dtoh(self.args[var_id], self.cu_args[var_id])
+		end = end.time()
+		self.dtoh_times.append(end - start)
 #		cuda.memcpy_dtoh(self.returns[i],self.cu_args[self.id_ret[i]])
 
 
